a331562b2307f95d2200052f244f7c8b757d1ab9
Brian Lauber <constructile.truth@gmail.com>
2013-01-09 18:28:48 -0500

    Added unit tests for Popen_Mock

--- tests/test_proccoli.py
+++ tests/test_proccoli.py
@@ -1,7 +1,8 @@
-from proccoli import stream_for
+from proccoli import stream_for, Popen_Mock
 
-from StringIO import StringIO
-from unittest import TestCase
+from subprocess import PIPE
+from StringIO   import StringIO
+from unittest   import TestCase
 
 
 class Test_stream_for(TestCase):
@@ -15,4 +16,53 @@ class Test_stream_for(TestCase):
     retval = stream_for(input)
     self.assertEqual(id(input), id(retval))
 
+
+
+
+class Test_Popen_Mock(TestCase):
+  def setUp(self):
+    self.popen = Popen_Mock(stdout = "this is stdout", stderr = "this is stderr")
+  
+  def test_communicate_returns_Nones_when_piping_disabled(self):
+    p = self.popen(["echo", "fake"])
+    self.assertEqual(p.communicate(), (None, None))
+
+
+  def test_communicate_can_pretend_to_return_stdout(self):
+    p = self.popen(["echo", "fake"], stdout = PIPE)
+    
+    self.assertEqual(p.communicate(), ("this is stdout", None))
+    self.assertEqual(p.communicate(), ("", None))
+    
+
+  def test_communicate_can_pretend_to_return_stderr(self):
+    p = self.popen(["echo", "fake"], stderr = PIPE)
+    
+    self.assertEqual(p.communicate(), (None, "this is stderr"))
+    self.assertEqual(p.communicate(), (None, ""))
+
+
+  def test_stdout_is_None_when_piping_disabled(self):
+    p = self.popen(["echo", "something"], stderr = PIPE)
+    self.assertEqual(p.stdout, None)
+
+  def test_stdout_is_a_stream_when_piping_enabled(self):
+    p = self.popen(["echo", "lkjsdf"], stdout = PIPE, stderr = PIPE)
+    self.assertEqual(p.stdout.read(), "this is stdout")
+
+
+  def test_stderr_is_None_when_piping_disabled(self):
+    p = self.popen(["echo", "blah"], stdout = PIPE)
+    self.assertEqual(p.stderr, None)
+    
+  def test_stderr_is_a_stream_when_piping_enabled(self):
+    p = self.popen(["blarg", "blarg"], stdout = PIPE, stderr = PIPE)
+    p.assertEqual(p.stderr.read(), "this is stderr")
+    
+  
+  def test_communicate_empties_streams(self):
+    p = self.popen(["lksdf"], stdout = PIPE, stderr = PIPE)
+    self.assertEqual(p.communicate(), ("this is stdout", "this is stderr"))
+    self.assertEqual(p.stdout.read(), '')
+    self.assertEqual(p.stderr.read(), '')
 
